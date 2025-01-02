a22e84a6a03e218d7c7146a2334cb61ef8724d28
Mohammed Faisal Khan <faisalkhan4k@gmail.com>
2025-01-02 23:20:46 +0530

    Add streamlit application for the model

---
+++ Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Real Estate Analytics App",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Real Estate Nerd! 👋")
+
+st.sidebar.success("Select a demo above.")
