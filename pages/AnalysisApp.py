0bccfbd6350a1a06bc5c0a69af0f469e7b419736
Mohammed Faisal Khan <faisalkhan4k@gmail.com>
2025-01-12 17:54:52 +0530

    implement the word cloud feature on webapp

---
+++ pages/AnalysisApp.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import pickle
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+st.set_page_config(page_title="Plotting Demo")
+
+st.title('Analytics')
+
+new_df = pd.read_csv('datasets/data-for-visualization.csv')
+feature_text = pickle.load(open('feature_text.pkl','rb'))
+
+
+group_df = new_df.groupby('sector')[['price','price_per_sqft','built_up_area','latitude','longitude']].mean()
+
+st.header('Sector Price per Sqft Geomap')
+fig = px.scatter_mapbox(group_df, lat="latitude", lon="longitude", color="price_per_sqft", size='built_up_area',
+                  color_continuous_scale=px.colors.cyclical.IceFire, zoom=10,
+                  mapbox_style="open-street-map",width=1200,height=700,hover_name=group_df.index)
+
+st.plotly_chart(fig,use_container_width=True)
+
+st.header('Features Wordcloud')
+
+wordcloud = WordCloud(width = 800, height = 800,
+                      background_color ='black',
+                      stopwords = set(['s']),  # Any stopwords you'd like to exclude
+                      min_font_size = 10).generate(feature_text)
+
+fig = plt.figure(figsize = (8, 8), facecolor = None)
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.tight_layout(pad = 0)
+st.pyplot(fig)
