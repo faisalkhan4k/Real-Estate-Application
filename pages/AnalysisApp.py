da3ba613e66aac03c5a9d5ac35e8c67985e444f8
Mohammed Faisal Khan <faisalkhan4k@gmail.com>
2025-01-13 13:15:11 +0530

    implement Pie Chart for number of bedrooms in each sector

--- pages/AnalysisApp.py
+++ pages/AnalysisApp.py
@@ -35,3 +35,60 @@ plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
 plt.tight_layout(pad = 0)
 st.pyplot(fig)
+
+st.header('Area Vs Price')
+
+property_type = st.selectbox('Select Property Type', ['flat','house'])
+
+if property_type == 'house':
+    fig1 = px.scatter(new_df[new_df['property_type'] == 'house'], x="built_up_area", y="price", color="bedRoom", title="Area Vs Price")
+
+    st.plotly_chart(fig1, use_container_width=True)
+else:
+    fig1 = px.scatter(new_df[new_df['property_type'] == 'flat'], x="built_up_area", y="price", color="bedRoom",
+                      title="Area Vs Price")
+
+    st.plotly_chart(fig1, use_container_width=True)
+
+st.header('BHK Pie Chart')
+
+sector_options = new_df['sector'].unique().tolist()
+sector_options.insert(0,'overall')
+
+selected_sector = st.selectbox('Select Sector', sector_options)
+
+if selected_sector == 'overall':
+
+    fig2 = px.pie(new_df, names='bedRoom')
+
+    st.plotly_chart(fig2, use_container_width=True)
+else:
+
+    fig2 = px.pie(new_df[new_df['sector'] == selected_sector], names='bedRoom')
+
+    st.plotly_chart(fig2, use_container_width=True)
+
+# st.header('Side by Side BHK price comparison')
+
+# fig3 = px.box(new_df[new_df['bedRoom'] <= 4], x='bedRoom', y='price', title='BHK Price Range')
+
+# st.plotly_chart(fig3, use_container_width=True)
+
+
+# st.header('Side by Side Distplot for property type')
+
+# fig3 = plt.figure(figsize=(10, 4))
+# sns.histplot(data=new_df[new_df['property_type'] == 'house'], x='price', kde=True, label='house', color='blue')
+# sns.histplot(data=new_df[new_df['property_type'] == 'flat'], x='price', kde=True, label='flat', color='orange')
+# plt.legend()
+# st.pyplot(fig3)
+
+
+
+
+
+
+
+
+
+
